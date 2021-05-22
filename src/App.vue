8130bdd1d83b6a4caa7291ca4ca792aeac150938
ZYROUGE <zyrouge@hotmail.com>
2021-05-22 19:55:02 +0530

    New downloads tab

--- src/App.vue
+++ src/App.vue
@@ -101,27 +101,76 @@
             <hr class="my-10" />
             <div>
                 <h1 class="text-2xl font-bold" id="download">Download</h1>
-                <div class="flex flex-row justify-center items-center mt-4">
+                <div class="mt-4" v-for="(download, i) in downloads">
+                    <p class="font-bold cursor-pointer" @click="setOpened(i)">
+                        <span class="mr-1.5 text-xs">
+                            <i
+                                class="fas fa-angle-double-down"
+                                v-if="isOpenedIndex === i"
+                            ></i>
+                            <i
+                                class="fas fa-angle-double-right"
+                                v-else
+                            ></i> </span
+                        >{{ download.version }}
+                    </p>
+                    <div
+                        class="
+                            flex flex-col
+                            lg:flex-row
+                            justify-center
+                            items-center
+                            gap-3
+                            mt-2
+                        "
+                        v-if="isOpenedIndex === i"
+                    >
+                        <a
+                            class="
+                                bg-indigo-500
+                                hover:bg-indigo-600
+                                text-white
+                                px-5
+                                py-2
+                                rounded-lg
+                                shadow-lg
+                                transition
+                                duration-300
+                            "
+                            :href="src.url"
+                            target="_blank"
+                            v-for="[platform, src] in Object.entries(
+                                download.platforms
+                            )"
+                        >
+                            <p>
+                                <span class="mr-1"
+                                    ><i :class="getIcon(platform)"></i
+                                ></span>
+                                Download for
+                                <span class="capitalize">{{ platform }}</span>
+                            </p>
+                            <p class="text-xs opacity-75">
+                                {{ getMb(src.size) }} MB
+                            </p>
+                        </a>
+                    </div>
+                </div>
+
+                <p class="mt-4">
                     <a
                         class="
-                            bg-indigo-500
-                            hover:bg-indigo-600
-                            text-white
-                            px-5
-                            py-2
-                            rounded-lg
-                            shadow-lg
+                            text-indigo-500
+                            hover:text-indigo-600
+                            font-bold
                             transition
                             duration-300
                         "
-                        href="https://github.com/zyrouge/yukino-app/releases"
+                        :href="releases.web"
                         target="_blank"
-                        ><span class="mr-1"
-                            ><i class="fab fa-windows"></i
-                        ></span>
-                        Download for Windows</a
+                        >View all releases</a
                     >
-                </div>
+                </p>
             </div>
 
             <hr class="my-10" />
@@ -130,7 +179,13 @@
                 <p class="mt-4">
                     Please read the
                     <a
-                        class="text-indigo-500 hover:text-indigo-600 font-bold"
+                        class="
+                            text-indigo-500
+                            hover:text-indigo-600
+                            font-bold
+                            transition
+                            duration-300
+                        "
                         href="https://github.com/zyrouge/yukino-app/#readme"
                         target="_blank"
                         >readme</a
@@ -147,7 +202,11 @@
 
             <p class="mt-1">
                 Made with <i class="fas fa-heart"></i> by
-                <a class="font-bold" href="https://zyrouge.is-a.dev" target="_blank">Zyrouge</a
+                <a
+                    class="font-bold"
+                    href="https://zyrouge.is-a.dev"
+                    target="_blank"
+                    >Zyrouge</a
                 >.
             </p>
         </div>
@@ -158,6 +217,17 @@
 import { defineComponent } from "vue";
 import NavBar from "./components/NavBar.vue";
 
+interface DownloadEntity {
+    version: string;
+    platforms: Record<
+        string,
+        {
+            size: number;
+            url: string;
+        }
+    >;
+}
+
 export default defineComponent({
     name: "App",
     components: {
@@ -167,6 +237,12 @@ export default defineComponent({
         const data: {
             screenshots: string[];
             features: string[];
+            releases: {
+                web: string;
+                all: string;
+            };
+            downloads: DownloadEntity[];
+            isOpenedIndex: number | null;
         } = {
             screenshots: [
                 "https://github.com/zyrouge/yukino-app/raw/next/screenshots/home.png",
@@ -185,10 +261,89 @@ export default defineComponent({
                 "Catchy but clean UI",
                 "Optimized for performance",
                 "Light and dark mode",
+                "Discord RPC",
             ],
+            releases: {
+                web: "https://github.com/zyrouge/yukino-app/releases",
+                all: "https://api.github.com/repos/zyrouge/yukino-app/releases",
+            },
+            downloads: [],
+            isOpenedIndex: 0,
         };
 
         return data;
+    },
+    mounted() {
+        this.getReleases();
+    },
+    methods: {
+        async getReleases() {
+            try {
+                const res = (await fetch(this.releases.all).then((res) =>
+                    res.json()
+                )) as any[];
+                const releases = res
+                    .filter((x) => !x.draft)
+                    .sort(
+                        (a, b) =>
+                            new Date(a.updated_at).getTime() -
+                            new Date(b.updated_at).getTime()
+                    );
+
+                releases.forEach((release) => {
+                    const version = release.tag_name;
+                    const windows = release.assets.find((x: any) =>
+                        x.browser_download_url.endsWith(".exe")
+                    );
+
+                    const linux = release.assets.find((x: any) =>
+                        x.browser_download_url.endsWith(".AppImage")
+                    );
+
+                    if (version) {
+                        const r: DownloadEntity = {
+                            version,
+                            platforms: {},
+                        };
+
+                        if (windows)
+                            r.platforms.windows = {
+                                url: windows.browser_download_url,
+                                size: windows.size,
+                            };
+
+                        if (linux)
+                            r.platforms.linux = {
+                                url: linux.browser_download_url,
+                                size: linux.size,
+                            };
+                        this.downloads.push(r);
+                    }
+                });
+            } catch (err) {
+                console.error(err);
+            }
+        },
+        setOpened(index: number) {
+            if (this.isOpenedIndex === index)
+                return (this.isOpenedIndex = null);
+            this.isOpenedIndex = index;
+        },
+        getMb(size: number) {
+            return (size / 1000000).toFixed(2);
+        },
+        getIcon(platform: string) {
+            switch (platform) {
+                case "windows":
+                    return "fab fa-windows";
+
+                case "linux":
+                    return "fab fa-linux";
+
+                default:
+                    return "";
+            }
+        },
     },
 });
 </script>
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    base: "/yukino-app",
+    base: "/yukino-app/",
     plugins: [vue()],
 });
